e60e2acb0a811b8bc9a54e55199dd33202d0cd81
之雨 <29673994+1299172402@users.noreply.github.com>
2023-07-23 16:16:26 +0800

    update: package in `--noconsole` mode

--- build-to-exe.py
+++ build-to-exe.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     main()
 
 # 打包本文件
-# pyinstaller --noconfirm --onefile --console --icon "./BBDown_GUI/UI/favicon.ico" --add-data "./BBDown_GUI/UI/favicon.ico;./UI"  "./build-to-exe.py"
+# pyinstaller --noconfirm --onefile --noconsole --icon "./BBDown_GUI/UI/favicon.ico" --add-data "./BBDown_GUI/UI/favicon.ico;./UI"  "./build-to-exe.py"
